fcbb08c1f378c9d1ba1fa8872206392ce3fb21fb
jwpettit <jwpettit@me.com>
2023-01-19 16:21:51 -0800

    Basic text content

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="px-4 py-5 my-5 text-center">
+      <div className="col-lg-6 mx-auto">
+        <h1>Hi, I'm Jeff!</h1>
+        <h1>Welcome to my site.</h1>
+        <br/>
+        <h2>I am a software engineer based in CA.</h2>
+        <h3>Please view my projects on my <a href="https://gitlab.com/jwpettit" target="blank">gitlab page</a>.</h3>
+      </div>
     </div>
   );
 }
-
-export default App;
