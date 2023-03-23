8f68593064589feed20b4853b3c36a9f079a5405
jwpettit <jwpettit@me.com>
2023-03-23 17:24:52 -0600

    Fixed issues around spacing of navlinks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,30 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
 
 export default function Navigation() {
     return (
         <Navbar>
             <Navbar.Collapse>
-                <Navbar.Brand
-                    as={NavLink}
-                    to="/"
-                >
-                    Jeff Pettit
-                </Navbar.Brand>
+                <Container>
+                    <Navbar.Brand as={NavLink} to="/">
+                        Jeff Pettit
+                    </Navbar.Brand>
+                </Container>
             </Navbar.Collapse>
             <Navbar.Collapse className="justify-content-end">
-                <Nav.Link as={NavLink} to="work/">Work</Nav.Link>
-                <Nav.Link as={NavLink} to="about/">About</Nav.Link>
-                <Nav.Link as={NavLink} to="contact/">Contact</Nav.Link>
+                <Container className="Navlinks-Box">
+                    <Nav.Link className="Navlinks" as={NavLink} to="work/">
+                        Work
+                    </Nav.Link>
+                    <Nav.Link className="Navlinks" as={NavLink} to="about/">
+                        About
+                    </Nav.Link>
+                    <Nav.Link className="Navlinks" as={NavLink} to="contact/">
+                        Contact
+                    </Nav.Link>
+                </Container>
             </Navbar.Collapse>
         </Navbar>
     );
