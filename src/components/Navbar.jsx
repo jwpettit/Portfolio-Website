92d46fc76356c8f6f59e03abcc9901de09f34dbb
jwpettit <jwpettit@me.com>
2023-03-23 11:51:14 -0700

    Added navlinks to navigation buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
@@ -5,14 +6,17 @@ export default function Navigation() {
     return (
         <Navbar>
             <Navbar.Collapse>
-                <Navbar.Brand>
+                <Navbar.Brand
+                    as={NavLink}
+                    to="/"
+                >
                     Jeff Pettit
                 </Navbar.Brand>
             </Navbar.Collapse>
             <Navbar.Collapse className="justify-content-end">
-                <Nav.Link>Work</Nav.Link>
-                <Nav.Link>About</Nav.Link>
-                <Nav.Link>Contact</Nav.Link>
+                <Nav.Link as={NavLink} to="work/">Work</Nav.Link>
+                <Nav.Link as={NavLink} to="about/">About</Nav.Link>
+                <Nav.Link as={NavLink} to="contact/">Contact</Nav.Link>
             </Navbar.Collapse>
         </Navbar>
     );
