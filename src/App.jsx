61e7e3be437f47c89bbfc0f91371a50726622ba8
jwpettit <jwpettit@me.com>
2023-03-23 11:31:18 -0700

    More page structuring

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,21 @@ import "./assets/fonts/DINR____.TTF";
 import "./assets/fonts/DINB____.TTF";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Templanding from "./pages/Templanding";
+import Landing from "./pages/Landing"
+import About from "./pages/About"
+import Invalid from "./pages/Invalid"
+
 
 export default function App() {
     return (
         <div className="App">
             <Routes>
-                <Route path="/" element={<Templanding />}></Route>
+                <Route path="/" element={<Layout
+                />} >
+                    <Route index element={<Landing />} />
+                    <Route path="about/" element={<Results />} />
+                    <Route path="*" element={<Invalid />} />
+                </Route>
             </Routes>
         </div>
     );
