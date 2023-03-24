902fa9b53817adc98fc1ba7b579a33fa829ca595
jwpettit <jwpettit@me.com>
2023-03-23 18:18:45 -0600

    Started landing page, fumbling around with ideas

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,3 +1,22 @@
-export default function Landing() {
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-};
+export default function Landing() {
+    return (
+        <div>
+            <Container>
+                <Row>
+                    <Col>
+                        <h1>Hi, I'm Jeff.</h1>
+                    </Col>
+                    <Col>
+                        <h3>
+                            A full-stack software engineer based in California.
+                        </h3>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+}
