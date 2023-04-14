360512281b5e9bae6a1fad6a0ed7308ea243bd52
jwpettit <jwpettit@me.com>
2023-04-14 16:20:57 -0700

    CSS progress, got the text and line working in the center of the page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -5,22 +5,17 @@ import Col from "react-bootstrap/Col";
 export default function Landing() {
     return (
         <div>
-            <Container>
-                <Row>
-                    <Col>
-                        <h1 className="Homepage-Intro-Text">Hi,</h1>
-                        <h1 className="Homepage-Intro-Text">I'm Jeff.</h1>
-                    </Col>
-                    <Col>
-                        <div className="Homepage-Divider-Line" />
-                    </Col>
-                    <Col>
-                        <h1 className="Homepage-About-Text">I'm</h1>
-                        <h1 className="Homepage-About-Text">a software engineer</h1>
-
-                    </Col>
-                </Row>
-            </Container>
+            <div className="Homepage-Container">
+                <div>
+                    <h1 className="Homepage-Intro-Text">Hi,</h1>
+                    <h1 className="Homepage-Intro-Text">I'm Jeff.</h1>
+                </div>
+                <div className="Vertical-Line" />
+                <div>
+                    <h1 className="Homepage-About-Text">I'm</h1>
+                    <h1 className="Homepage-About-Text">a software engineer</h1>
+                </div>
+            </div>
         </div>
     );
 }
