9405a3a3a143c613653d037a3900f6f6853f08d5
jwpettit <jwpettit@me.com>
2023-04-15 22:38:13 -0700

    Commented out the flags after feedback they indicate translation which I don't mean to do

--- src/components/Footerbar.jsx
+++ src/components/Footerbar.jsx
@@ -15,27 +15,27 @@ export default function Footer() {
             </Navbar.Collapse>
             <Navbar.Collapse className="justify-content-end">
                 <Container className="Footer-Flags-Box">
-                    <Navbar.Brand href="">
+                    {/* <Navbar.Brand href="">
                         <img
                             alt="USA Flag"
                             src={`${process.env.PUBLIC_URL}/flags/Flag_USA.svg`}
                             height="60"
                         />
-                    </Navbar.Brand>
-                    <Navbar.Brand href="">
+                    </Navbar.Brand> */}
+                    {/* <Navbar.Brand href="">
                         <img
                             alt="Luxembourg Flag"
                             src={`${process.env.PUBLIC_URL}/flags/Flag_Luxembourg.svg`}
                             height="60"
                         />
-                    </Navbar.Brand>
-                    <Navbar.Brand href="">
+                    </Navbar.Brand> */}
+                    {/* <Navbar.Brand href="">
                         <img
                             alt="LGBTQ Flag"
                             src={`${process.env.PUBLIC_URL}/flags/Flag_LGBT.svg`}
                             height="60"
                         />
-                    </Navbar.Brand>
+                    </Navbar.Brand> */}
                 </Container>
             </Navbar.Collapse>
         </Navbar>
