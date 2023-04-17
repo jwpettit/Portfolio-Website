521c7b042fb46d8113804f78f60e0e8dddaa4a82
jwpettit <jwpettit@me.com>
2023-04-17 15:36:18 -0700

    Resume section Headers

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -14,89 +14,21 @@ export default function Resume() {
 
             <div className="Resume-Body-Container">
                 <div className="Resume-Main-Column">
-                    <p className="Body-Text">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Donec eget dui nec velit mattis dictum. Quisque accumsan
-                        malesuada neque, non ornare ligula rutrum vitae. Fusce
-                        nec tristique velit. Ut euismod diam orci, ut bibendum
-                        dui gravida id. Ut ultricies ex ut nisl condimentum,
-                        quis pretium sem tempus. Aenean vulputate augue eu
-                        mauris fringilla, sed bibendum magna finibus. Proin
-                        velit nisi, blandit in semper at, aliquam eget tortor.
-                        Interdum et malesuada fames ac ante ipsum primis in
-                        faucibus. Vivamus in porttitor ante. Maecenas ipsum
-                        dolor, dapibus vestibulum ante eget, auctor gravida ex.
-                        Ut placerat enim sit amet magna convallis ultrices.
-                        Aenean a pulvinar ante. Nullam ut enim ultricies,
-                        iaculis justo vel, imperdiet metus. Maecenas id rhoncus
-                        neque. Aliquam at tincidunt nisl, ut sollicitudin dui.
-                        Curabitur vehicula sit amet mi sit amet posuere.
-                        Phasellus a risus aliquet nibh rutrum iaculis. Donec
-                        imperdiet, ipsum laoreet porttitor mollis, sapien nisl
-                        lacinia neque, ac euismod velit justo vel augue. Morbi
-                        nec ultricies enim. Vivamus commodo, augue in dignissim
-                        fringilla, orci velit efficitur leo, a pretium nunc diam
-                        et tortor. Vivamus vel ante eget enim lobortis
-                        condimentum id quis ex. Morbi sit amet ultrices erat,
-                        sit amet tincidunt mauris. Nullam non metus eu risus
-                        maximus ullamcorper. Etiam feugiat in turpis a
-                        efficitur. Sed neque libero, hendrerit eget nibh in,
-                        laoreet laoreet neque. Aliquam nisl lacus, porta et
-                        tellus ac, pellentesque ullamcorper turpis. Quisque
-                        augue nunc, congue et risus non, facilisis porttitor
-                        elit. Nulla dictum purus sed pellentesque mattis.
-                        Aliquam a leo quis magna ullamcorper bibendum. Cras
-                        ultrices nisl sed tellus pharetra, nec posuere sapien
-                        auctor. Ut in laoreet metus. Ut sagittis aliquet
-                        interdum. Nam congue ex non ante cursus, at maximus
-                        felis accumsan. Mauris efficitur aliquam dui, in
-                        tincidunt neque viverra in. Praesent ipsum sapien,
-                        pellentesque eget tortor tempus, vestibulum tempus
-                        dolor. Ut non eleifend metus. Lorem ipsum dolor sit
-                        amet, consectetur adipiscing elit. Phasellus eget metus
-                        orci. Nam vulputate imperdiet libero, et bibendum ante
-                        mollis eu. Fusce aliquet sem erat, et dignissim neque
-                        cursus quis. Lorem ipsum dolor sit amet, consectetur
-                        adipiscing elit. Maecenas vestibulum neque a facilisis
-                        rutrum. Integer sit amet risus in justo ullamcorper
-                        sollicitudin. Class aptent taciti sociosqu ad litora
-                        torquent per conubia nostra, per inceptos himenaeos.
-                        Nunc maximus interdum ipsum dignissim posuere. Interdum
-                        et malesuada fames ac ante ipsum primis in faucibus.
-                        Aliquam rutrum mauris ex, ac varius nibh volutpat vitae.
-                        Nam elementum, magna sit amet malesuada tristique, ipsum
-                        orci aliquam orci, sed tincidunt diam ante in ex. Sed
-                        eget massa ligula. Morbi tempor cursus est at viverra.
-                        Aenean ornare quis neque id sollicitudin. Etiam risus
-                        ante, pretium id porttitor tempor, venenatis ut nunc.
-                        Vestibulum semper venenatis sodales.
-                    </p>
+                    <div className="H4-Header">
+                        <h4>Application Development Experience</h4>
+                    </div>
+                    <div className="H4-Header">
+                        <h4>Professional Experience</h4>
+                    </div>
                 </div>
+
                 <div className="Resume-Secondary-Column">
-                    <p className="Body-Text">
-                        Nullam scelerisque nisi eget lectus dignissim ultrices.
-                        Nullam ultricies, nulla sed pretium scelerisque, dolor
-                        lorem commodo ante, et vestibulum dui urna nec diam.
-                        Vivamus tincidunt gravida ultricies. Nam a euismod nibh.
-                        Proin vel odio sit amet massa fermentum feugiat. Nunc
-                        urna felis, varius sit amet dolor sit amet, porttitor
-                        vehicula arcu. Aliquam fermentum porta dignissim.
-                        Maecenas accumsan lacinia fringilla. Nam vel risus in
-                        elit hendrerit suscipit. Cras odio turpis, dignissim eu
-                        suscipit consequat, consequat nec dolor. Vivamus et ante
-                        in metus tempor tristique pulvinar id sem. Suspendisse
-                        sit amet fringilla quam. Etiam ut venenatis tellus, non
-                        ultrices ipsum. Duis quis elit sem. Proin sit amet arcu
-                        dapibus, luctus augue nec, eleifend nunc. Cras nisl
-                        diam, egestas in mattis a, auctor sit amet libero. Etiam
-                        elementum commodo felis ac euismod. Nunc a mattis est,
-                        eget condimentum velit. In vestibulum metus sagittis
-                        eleifend pretium. Etiam suscipit lectus eget lectus
-                        efficitur, a lacinia lorem malesuada. Praesent
-                        consectetur rutrum nunc vel porttitor. Nulla sed velit
-                        id augue pellentesque vehicula et sed lorem. Nullam
-                        auctor ornare massa id egestas.
-                    </p>
+                    <div className="H4-Header">
+                        <h4>Education</h4>
+                    </div>
+                    <div className="H4-Header">
+                        <h4>Technical Skills</h4>
+                    </div>
                 </div>
             </div>
         </div>
