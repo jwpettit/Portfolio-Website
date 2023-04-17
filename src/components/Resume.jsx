94418404115c163b63e5370c1a34f8b3bd6d88b4
jwpettit <jwpettit@me.com>
2023-04-17 16:28:49 -0700

    Completed all work experience in resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -17,6 +17,7 @@ export default function Resume() {
                     <div className="H4-Header">
                         <h4>Application Development Experience</h4>
                     </div>
+
                     <div className="Resume-Header-Container">
                         <div className="Resume-Header-Title-Container">
                             <div className="Resume-Body-Text-Bold">
@@ -156,7 +157,6 @@ export default function Resume() {
                             </li>
                         </ul>
                     </div>
-
                     <div className="Resume-Header-Container">
                         <div className="Resume-Header-Title-Container">
                             <div className="Resume-Body-Text-Bold">
@@ -209,6 +209,140 @@ export default function Resume() {
                                 milestones, sharing updates with stakeholders
                                 and tracking development progress against
                                 timelines
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="Resume-Header-Container">
+                        <div className="Resume-Header-Title-Container">
+                            <div className="Resume-Body-Text-Bold">
+                                <p>Super7</p>
+                            </div>
+                            <div className="Resume-Text-Divider" />
+                            <div className="Resume-Body-Text">
+                                <p>Product Design Engineer</p>
+                            </div>
+                            <div className="Resume-Text-Divider" />
+                            <div className="Resume-Body-Text">
+                                <p>San Francisco, CA</p>
+                            </div>
+                        </div>
+                        <div className="Resume-Body-Text-Right">
+                            <p>2018-2021</p>
+                        </div>
+                    </div>
+                    <div className="Resume-Body-Text">
+                        <ul className="Resume-List-Margin">
+                            <li>
+                                Introduced advanced prototyping techniques, such
+                                as: in-house 3D modeling, 3D printing, and
+                                mechanical stress-testing, defined detailed
+                                manufacturing specifications, and implemented
+                                development best-practices in order to lower
+                                product defect rates, deliver product on-time
+                                and on-budget
+                            </li>
+                            <li>
+                                Led transition to new bigger production
+                                facility, after reviewing the current supply
+                                chain strategy, in order to benefit from
+                                improved efficiencies through increased
+                                automation{" "}
+                            </li>
+                            <li>
+                                Directed manufacturers to follow strict brand
+                                guidelines, ensuring the highest quality of
+                                licensed product lines with Disney,
+                                NBCUniversal, 21st Century Fox, MLB, NBA, and
+                                more
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="Resume-Header-Container">
+                        <div className="Resume-Header-Title-Container">
+                            <div className="Resume-Body-Text-Bold">
+                                <p>Piper</p>
+                            </div>
+                            <div className="Resume-Text-Divider" />
+                            <div className="Resume-Body-Text">
+                                <p>Lead Industrial Designer</p>
+                            </div>
+                            <div className="Resume-Text-Divider" />
+                            <div className="Resume-Body-Text">
+                                <p>San Francisco, CA</p>
+                            </div>
+                        </div>
+                        <div className="Resume-Body-Text-Right">
+                            <p>2017-2018</p>
+                        </div>
+                    </div>
+                    <div className="Resume-Body-Text">
+                        <ul className="Resume-List-Margin">
+                            <li>
+                                Led entire product development of the 2nd
+                                generation Piper Computer Kit: from identifying
+                                common product failure points from extensive
+                                user-testing, through innovative hardware UX,
+                                resulting in a successful relaunch of the
+                                product
+                            </li>
+                            <li>
+                                Conducted frequent user-testing of Piper’s
+                                proprietary mechanical build and software
+                                experiences including games, coding, and
+                                engineering principles of physics, mechanics,
+                                electronics, and circuits
+                            </li>
+                            <li>
+                                Created end-to-end user journey through visual
+                                documentation consisting of highly-annotated
+                                technical drawings presented in an accessible,
+                                easy-to-understand blueprint
+                            </li>
+                            <li>
+                                Partnered closely with contract manufacturers,
+                                traveling multiple times overseas, to produce
+                                and ship in time to meet the demand of the 2018
+                                holiday season
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="Resume-Header-Container">
+                        <div className="Resume-Header-Title-Container">
+                            <div className="Resume-Body-Text-Bold">
+                                <p>KiwiCo (Tinker Crate)</p>
+                            </div>
+                            <div className="Resume-Text-Divider" />
+                            <div className="Resume-Body-Text">
+                                <p>Industrial Designer</p>
+                            </div>
+                            <div className="Resume-Text-Divider" />
+                            <div className="Resume-Body-Text">
+                                <p>Mountain View, CA</p>
+                            </div>
+                        </div>
+                        <div className="Resume-Body-Text-Right">
+                            <p>2015-2017</p>
+                        </div>
+                    </div>
+                    <div className="Resume-Body-Text">
+                        <ul className="Resume-List-Margin">
+                            <li>
+                                Designed, engineered, and prototyped STEM
+                                products and experiences for the Tinker Crate
+                                brand under a 6-month product development
+                                lifecycle
+                            </li>
+                            <li>
+                                Shipped 17+ products in a 3-year time span,
+                                value-engineering against aggressive margin
+                                goals, while conducting extensive user testing
+                                to achieve consistently high NPS scores
+                            </li>
+                            <li>
+                                Managed editorial team to publish technical
+                                instructions, supplementing the products to
+                                ensure a positive experience for end users age
+                                9+
                             </li>
                         </ul>
                     </div>
