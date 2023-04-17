e540fe53bd35f829ed1cb31638ed831862279c9f
jwpettit <jwpettit@me.com>
2023-04-17 11:34:46 -0700

    Some more CSS layout progress on the contact page, I'm learning

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,15 @@ import ContactForm from "../components/ContactForm";
 export default function Contact() {
     return (
         <div className="Page-Background-Container">
-            <ContactForm />
+            <div className="Contact-Container-1">
+                <div className="H1-Header">
+                    <h1>Let's work together!</h1>
+                </div>{" "}
+                <div className="H3-Header">
+                    <h3>Contact me:</h3>
+                </div>
+                <ContactForm />
+            </div>
         </div>
     );
 }
