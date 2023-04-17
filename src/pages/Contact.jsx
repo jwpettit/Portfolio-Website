5a0a69a4c67040d506245f5eb31acfbcdad472ba
jwpettit <jwpettit@me.com>
2023-04-17 11:48:28 -0700

    Contact page coming together

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,14 +3,26 @@ import ContactForm from "../components/ContactForm";
 export default function Contact() {
     return (
         <div className="Page-Background-Container">
-            <div className="Contact-Container-1">
+            <div className="Page-Content-Container-1">
                 <div className="H1-Header">
                     <h1>Let's work together!</h1>
-                </div>{" "}
+                </div>
                 <div className="H3-Header">
                     <h3>Contact me:</h3>
                 </div>
-                <ContactForm />
+                <div className="Contact-Container-Flex">
+                    <div className="Contact-Column">
+                        <p className="Body-Text">
+                            I'm currently seeking roles in software
+                            engineering/development that are based in California
+                            or remote. Also interested to chat with anyone! Want
+                            to get in touch? Fill out the form to contact me!
+                        </p>
+                    </div>
+                    <div className="Contact-Column">
+                        <ContactForm />
+                    </div>
+                </div>
             </div>
         </div>
     );
