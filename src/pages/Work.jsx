0af0710925118efa5ddf43e6b83dd6c3f8937593
jwpettit <jwpettit@me.com>
2023-04-17 21:34:49 -0700

    Started roughing out work page

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,14 +1,111 @@
+import AboutMe from "../components/AboutMe";
+import Resume from "../components/Resume";
+
 export default function Work() {
     return (
-        <div className="Invalid-Container">
-            <div className="H1-Header">
-                <h1>Work</h1>
-                <h1>This page is under construction.</h1>
-            </div>
-            <br />
-            <div className="H3-Header">
-                <h3>Don't worry, it'll be done very soon!</h3>
+        <div className="Page-Background-Container">
+            <div className="Page-Content-Container-1">
+                <div className="H1-Header">
+                    <h1>Work</h1>
+                </div>
+                <br />
+                <div className="H3-Header">
+                    <h3>
+                        utrum nunc vel porttitor. Nulla sed velit id augue
+                        pellentesque vehicula et sed lorem. Nullam auctor ornare
+                        massa id egestas. Curabitur at risus nec dolor laoreet
+                        ultrices.
+                    </h3>
+                </div>
+                <div className="Horizontal-Section-Divider" />
+                <div className="Page-Content-Container-2">
+                    <div className="Page-Content-Column-1"></div>
+                    <div className="Page-Content-Column-1">
+                        <div className="H3-Header">
+                            <h3>BrewHop</h3>
+                        </div>
+                        <br />
+                        <div className="Body-Text">
+                            <p>
+                                Sed luctus lacus eu semper feugiat. Vestibulum
+                                aliquam fermentum elit eu rhoncus. Cras cursus,
+                                lorem ut molestie ultricies, mauris nisl
+                                ullamcorper dolor, vel ullamcorper nulla nisl at
+                                ligula. Sed placerat sem arcu. Cras quis diam
+                                tortor. Mauris sed auctor nisi. Maecenas
+                                imperdiet facilisis ipsum, id laoreet diam
+                                placerat nec.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="Horizontal-Section-Divider" />
+                <div className="Page-Content-Container-2">
+                    <div className="Page-Content-Column-1"></div>
+                    <div className="Page-Content-Column-1">
+                        <div className="H3-Header">
+                            <h3>Treampardy</h3>
+                        </div>
+                        <br />
+                        <div className="Body-Text">
+                            <p>
+                                Sed luctus lacus eu semper feugiat. Vestibulum
+                                aliquam fermentum elit eu rhoncus. Cras cursus,
+                                lorem ut molestie ultricies, mauris nisl
+                                ullamcorper dolor, vel ullamcorper nulla nisl at
+                                ligula. Sed placerat sem arcu. Cras quis diam
+                                tortor. Mauris sed auctor nisi. Maecenas
+                                imperdiet facilisis ipsum, id laoreet diam
+                                placerat nec.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="Horizontal-Section-Divider" />
+                <div className="Page-Content-Container-2">
+                    <div className="Page-Content-Column-1"></div>
+                    <div className="Page-Content-Column-1">
+                        <div className="H3-Header">
+                            <h3>AutoBahn</h3>
+                        </div>
+                        <br />
+                        <div className="Body-Text">
+                            <p>
+                                Sed luctus lacus eu semper feugiat. Vestibulum
+                                aliquam fermentum elit eu rhoncus. Cras cursus,
+                                lorem ut molestie ultricies, mauris nisl
+                                ullamcorper dolor, vel ullamcorper nulla nisl at
+                                ligula. Sed placerat sem arcu. Cras quis diam
+                                tortor. Mauris sed auctor nisi. Maecenas
+                                imperdiet facilisis ipsum, id laoreet diam
+                                placerat nec.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="Horizontal-Section-Divider" />
+                <div className="Page-Content-Container-2">
+                    <div className="Page-Content-Column-1"></div>
+                    <div className="Page-Content-Column-1">
+                        <div className="H3-Header">
+                            <h3>Portfolio Site</h3>
+                        </div>
+                        <br />
+                        <div className="Body-Text">
+                            <p>
+                                Sed luctus lacus eu semper feugiat. Vestibulum
+                                aliquam fermentum elit eu rhoncus. Cras cursus,
+                                lorem ut molestie ultricies, mauris nisl
+                                ullamcorper dolor, vel ullamcorper nulla nisl at
+                                ligula. Sed placerat sem arcu. Cras quis diam
+                                tortor. Mauris sed auctor nisi. Maecenas
+                                imperdiet facilisis ipsum, id laoreet diam
+                                placerat nec.
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
-};
+}
