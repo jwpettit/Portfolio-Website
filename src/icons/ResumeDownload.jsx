f5db098790b9142ab23425c0f71ef370d5fc88c3
jwpettit <jwpettit@me.com>
2023-04-17 17:31:57 -0700

    Added resume file and corrected code for download button

--- src/icons/ResumeDownload.jsx
+++ src/icons/ResumeDownload.jsx
@@ -4,8 +4,8 @@ import DownloadIcon from '@mui/icons-material/Download';
 export default function DownloadResumeIcon() {
     return (
         <StyledIconButton
-            href={`${process.env.PUBLIC_URL}/flags/Flag_USA.svg`}
-            download=""
+            href={`${process.env.PUBLIC_URL}/Jeff Pettit_Resume_SPRING 23.pdf`}
+            download="Jeff Pettit_Resume_SPRING 23.pdf"
         >
             <DownloadIcon className="Resume-Download-Icon" />
         </StyledIconButton>
