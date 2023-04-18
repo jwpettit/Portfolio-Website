f720e254621c00a05ab78610a502abb8121a9e10
jwpettit <jwpettit@me.com>
2023-04-18 14:16:39 -0700

    About me tweaking

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -20,13 +20,12 @@ export default function AboutMe() {
                     <br />
                     <div className="H3-Header">
                         <h3>
-                            I'm passionate about crafting great products and
-                            experiences.
+                            I craft products and experiences.
                         </h3>
                     </div>
                     <br />
                     <div className="Body-Text">
-                        <p>
+                        {/* <p>
                             Through my experience working as an industrial
                             designer, I’ve come to realize that I am naturally a
                             detail-oriented problem-solver who is passionate
@@ -43,6 +42,24 @@ export default function AboutMe() {
                             technical skills. I'm always learning and lately
                             have been focused on getting into mobile
                             development.
+                        </p> */}
+                        <p>
+                            I am a detail-oriented problem-solver with a passion
+                            for technology and a focus on user-centered design.
+                            With a background in industrial design, I have
+                            experience managing projects with diverse
+                            stakeholders both domestically and abroad. During my
+                            time at Hack Reactor, I gained valuable technical
+                            skills in software engineering, including experience
+                            with JavaScript, React, Python, and Django.
+                        </p>
+                        <p>
+                            More recently, I have been focused on mobile
+                            development, learning and exploring frameworks such
+                            as SwiftUI and React Native. I am committed to
+                            staying up-to-date with the latest trends and
+                            technologies in software engineering, and am excited
+                            to bring my skills and passion to a new role.
                         </p>
                     </div>
                 </div>
